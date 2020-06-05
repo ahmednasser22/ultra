3f44ff4630b383323437d1edaf2ec4bfdadfd4e6
AhmedNasser-Front-End <ahmednasser_8@yahoo.com>
2020-06-05 23:43:16 +0200

    Updates

---
+++ precache-manifest.6c55c28fe7d0704ff03af472b0aec435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2bada59a63a8d87c8d1e187f7099e5",
+    "url": "/ultra/index.html"
+  },
+  {
+    "revision": "f3c43799ce0f71101433",
+    "url": "/ultra/static/css/main.ca3ab970.chunk.css"
+  },
+  {
+    "revision": "c2e83ae538b0c4adaf1a",
+    "url": "/ultra/static/js/2.c5d8b5a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ultra/static/js/2.c5d8b5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c43799ce0f71101433",
+    "url": "/ultra/static/js/main.7d5a2586.chunk.js"
+  },
+  {
+    "revision": "0a4523bd2bd9799cb6dc",
+    "url": "/ultra/static/js/runtime-main.0124bf8b.js"
+  }
+]);
